d443705ce6e52a8475d7423ae8f56123c31362c5
xakimovallamurod <133854536+xakimovallamurod@users.noreply.github.com>
2025-02-24 06:33:07 +0200

    get users datas

---
+++ main.py
@@ -0,0 +1,66 @@
+
+import csv
+
+class Users(object):
+    def __init__(self, file_path):
+        
+        # open file 
+        # Read and parse the CSV file
+        pass
+
+    def first_name_all(slef):
+
+        # Users data parse
+
+        # retrun first_name all list type
+        
+        pass
+
+    def last_name_all(slef):
+
+        # Users data parse
+
+        # retrun last_name all list type
+        
+        pass
+
+    def gender_male_count(slef):
+
+        # Users data parse
+
+        # retrun gender mael count 
+        
+        pass
+
+    def gender_female_count(slef):
+
+        # Users data parse
+
+        # retrun gender femael count 
+        
+        pass
+
+    def get_all_domen_name(slef):
+
+        # Users data parse
+
+        # Return all unique domain names from the data.
+
+        # sample: username@gmail.com -> domen_name = 'gmail'
+
+        pass
+
+    def get_job(slef):
+
+        # Get all unique jobs from user data
+
+        # Return answer
+
+        pass
+
+    def get_one_user(self, id: int):
+
+        # Return the user ID data equal to the id value.
+        pass
+    
+
